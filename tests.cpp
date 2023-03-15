be72858776ded54d6ef2626eb12f3e90b6714246
ethanhuang <ethanhuang0606@gmail.com>
2023-03-14 23:01:31 -0700

    update

--- tests.cpp
+++ tests.cpp
@@ -1,23 +1,23 @@
-#define CATCH_CONFIG_MAIN
+// #define CATCH_CONFIG_MAIN
 #include <catch2/catch_test_macros.hpp>
 
 unsigned int Factorial(unsigned int number) {
   return number <= 1 ? 1 : Factorial(number - 1) * number;
 }
 
-// TEST_CASE("Fac 0 and 1", "[factorial]") {
-//   REQUIRE(Factorial(0) == 1);
-//   REQUIRE(Factorial(1) == 1);
-// }
-//
-// TEST_CASE("Fac 2 and 3", "[factorial]") {
-//   REQUIRE(Factorial(2) == 2);
-//   REQUIRE(Factorial(3) == 6);
-// }
-//
-// TEST_CASE("Fac more", "[factorial]") {
-//   REQUIRE(Factorial(10) == 3628800);
-// }
+TEST_CASE("Fac 0 and 1", "[factorial]") {
+  REQUIRE(Factorial(0) == 1);
+  REQUIRE(Factorial(1) == 1);
+}
+
+TEST_CASE("Fac 2 and 3", "[factorial]") {
+  REQUIRE(Factorial(2) == 2);
+  REQUIRE(Factorial(3) == 6);
+}
+
+TEST_CASE("Fac more", "[factorial]") {
+  REQUIRE(Factorial(10) == 3628800);
+}
 
 TEST_CASE("vectors can be sized and resized", "[vector]") {
 
